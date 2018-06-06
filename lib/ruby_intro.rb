b3e24203eb69bf8c57a756bbe081b6094434b3fc
Hari Govind <hgs@disroot.org>
2018-06-06 23:59:12 +0530

    Completed Second Part

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -24,15 +24,25 @@ end
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s =~ /^[^aeiou].*/i && s=~ /^[a-z].*/i
+    return true
+  else
+    return false
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return false unless s =~ /^[01]+/
+  s = s.to_i(2)
+  if s%4 == 0
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
